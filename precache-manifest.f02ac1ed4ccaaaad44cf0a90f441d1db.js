a9f916af389b4f4934adcdd13dfe69dc596170f2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-13 22:12:51 +0000

    Deploy kevin-k/resume-react to github.com/kevin-k/resume-react.git:gh-pages

---
+++ precache-manifest.f02ac1ed4ccaaaad44cf0a90f441d1db.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d4632eb2fd7d3cf26ec04e419e90a2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.55d14011967a8f7adeb4.bundle.js"
+  },
+  {
+    "url": "runtime~main.55d14011967a8f7adeb4.bundle.js"
+  },
+  {
+    "url": "vendors~main.55d14011967a8f7adeb4.bundle.js"
+  },
+  {
+    "url": "vendors~main.55d14011967a8f7adeb4.bundle.js.LICENSE.txt"
+  }
+]);
